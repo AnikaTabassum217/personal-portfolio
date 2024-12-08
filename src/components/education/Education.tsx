bf6505fe00e2ae0c9d727624c07583e7656b7bc9
AnikaTabassum217 <tabassum.anika579@gmail.com>
2024-12-08 14:25:59 -0800

    update code

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -1,31 +1,21 @@
-"use client";
+// import PdfViewer from "./PdfViewer";
 import EducationBackground from "./EducationBackground";
 import EducationDetails from "./EducationDetails";
-import PdfViewer from "./PdfViewer"
-
 
 const Education = () => {
-    return (
-        <>
-          
-
-<div className="flex flex-col ">
-      {/* Background Section */}
+  return (
+    <div className="flex flex-col">
       <div className="relative w-full h-screen ">
         <EducationBackground />
       </div>
-
-      {/* Details Section */}
       <div className="w-full">
         <EducationDetails />
       </div>
-
       <div>
-      <PdfViewer url={"/images/education/cv.pdf"} />    
-        </div>
+        {/* <PdfViewer url={"/images/education/cv.pdf"} /> */}
+      </div>
     </div>
-          
-        </>
-    )
-}
-export default Education
+  );
+};
+
+export default Education;
