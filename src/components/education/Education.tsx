fc9832347f59a74e548181dd007a4da1e959afec
AnikaTabassum217 <tabassum.anika579@gmail.com>
2024-12-22 16:17:27 -0800

    PDF added

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -1,18 +1,23 @@
-import PdfViewer from "./PdfViewer";
 import EducationBackground from "./EducationBackground";
 import EducationDetails from "./EducationDetails";
 
 const Education = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="relative w-full h-screen ">
         <EducationBackground />
       </div>
       <div className="w-full">
         <EducationDetails />
       </div>
-      <div>
-        <PdfViewer url={"/images/education/cv.pdf"} />
+
+      <div className="container mx-auto">
+        <iframe
+          src="/images/education/cv.pdf"
+          width={1000}
+          height={1000}
+          className="h-screen "
+        />
       </div>
     </div>
   );
--- src/components/education/PdfViewer.jsx
+++ src/components/education/PdfViewer.jsx
@@ -1,38 +1,61 @@
+// // "use client";
+// // import { Viewer, Worker } from "@react-pdf-viewer/core";
+// // import "@react-pdf-viewer/core/lib/styles/index.css";
+// // import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+// // import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+// // const PdfViewer = ({ url }) => {
+// //   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+// //   return (
+// //     <div className="h-screen w-screen">
+// //       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js">
+// //         <Viewer
+// //           fileUrl={url}
+// //           plugins={[defaultLayoutPluginInstance]}
+// //         />
+// //       </Worker>
+// //     </div>
+// //   );
+// // };
+// // export default PdfViewer;
+
+
+// // "use client";
+
+// // import { Viewer, Worker } from "@react-pdf-viewer/core";
+// // import "@react-pdf-viewer/core/lib/styles/index.css"; // Core styles for the viewer
+// // import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+// // import "@react-pdf-viewer/default-layout/lib/styles/index.css"; // Styles for the default layout plugin
+
+// // const PdfViewer = ({ url }) => {
+// //   // Initialize the default layout plugin
+// //   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+// //   return (
+// //     <div className="h-screen w-screen">
+// //       {/* Set the worker URL for PDF.js */}
+// //       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+// //         <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
+// //       </Worker>
+// //     </div>
+// //   );
+// // };
+
+// // export default PdfViewer;
+
+
 // "use client";
+
 // import { Viewer, Worker } from "@react-pdf-viewer/core";
 // import "@react-pdf-viewer/core/lib/styles/index.css";
 // import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 // import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-// const PdfViewer = ({ url }) => {
-//   const defaultLayoutPluginInstance = defaultLayoutPlugin();
-//   return (
-//     <div className="h-screen w-screen">
-//       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js">
-//         <Viewer
-//           fileUrl={url}
-//           plugins={[defaultLayoutPluginInstance]}
-//         />
-//       </Worker>
-//     </div>
-//   );
-// };
-// export default PdfViewer;
-
-
-// "use client";
-
-// import { Viewer, Worker } from "@react-pdf-viewer/core";
-// import "@react-pdf-viewer/core/lib/styles/index.css"; // Core styles for the viewer
-// import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-// import "@react-pdf-viewer/default-layout/lib/styles/index.css"; // Styles for the default layout plugin
 
 // const PdfViewer = ({ url }) => {
-//   // Initialize the default layout plugin
 //   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
 //   return (
 //     <div className="h-screen w-screen">
-//       {/* Set the worker URL for PDF.js */}
+//       {/* Updated Worker URL to match the installed version */}
 //       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
 //         <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
 //       </Worker>
@@ -41,26 +64,3 @@
 // };
 
 // export default PdfViewer;
-
-
-"use client";
-
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-
-const PdfViewer = ({ url }) => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
-  return (
-    <div className="h-screen w-screen">
-      {/* Updated Worker URL to match the installed version */}
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
-      </Worker>
-    </div>
-  );
-};
-
-export default PdfViewer;
